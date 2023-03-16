53e25ec99da90a4f45bc77adf7520c7883f21ac7
sadmann7 <sadmann898@gmail.com>
2023-03-16 16:08:59 +0600

    update og:image

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -23,20 +23,32 @@ export default function handler(req: NextRequest) {
 
     return new ImageResponse(
       (
-        <div tw="bg-black bg-cover h-full w-full flex items-center justify-center flex-col">
+        <div
+          tw="h-full w-full flex items-center justify-center flex-col"
+          style={{
+            backgroundImage: "linear-gradient(to bottom, #111827, #374151)",
+          }}
+        >
           <div tw="flex items-center text-3xl justify-center flex-col">
-            {
-              // eslint-disable-next-line @next/next/no-img-element
-              <img
-                alt="Vercel"
-                height={100}
-                src="data:image/svg+xml,%3Csvg width='116' height='100' fill='white' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M57.5 0L115 100H0L57.5 0z' /%3E%3C/svg%3E"
-                width={116}
-                className="my-8"
-              />
-            }
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="125"
+              height="125"
+              viewBox="0 0 25 25"
+              fill="transparent"
+              stroke="white"
+              stroke-width="1"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="feather feather-package"
+            >
+              <line x1="16.5" y1="9.4" x2="7.5" y2="4.21"></line>
+              <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path>
+              <polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline>
+              <line x1="12" y1="22.08" x2="12" y2="12"></line>
+            </svg>
           </div>
-          <div tw="flex max-w-2xl items-center justify-center flex-col mt-10">
+          <div tw="flex max-w-4xl items-center justify-center flex-col mt-10">
             <div tw="text-5xl font-bold whitespace-pre-wrap tracking-tight leading-tight text-white px-8">
               {title}
             </div>
